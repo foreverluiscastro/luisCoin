811943ff7b1f74c79c0241e91305baaafaf8167e
luis <foreverluiscastro@gmail.com>
2021-09-20 15:43:59 -0400

    tampered with data and hash to check validity.

--- main.js
+++ main.js
@@ -32,10 +32,36 @@ class Blockchain{
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 let luisCoin = new Blockchain();
 luisCoin.addBlock(new Block(1, "06/07/2021", { amount: 4 }));
 luisCoin.addBlock(new Block(2, "07/08/2021", { amount: 10 }));
 
-console.log(JSON.stringify(luisCoin, null, 4));
+console.log('Is blockchain valid?' + luisCoin.isChainValid());
+
+// console.log(JSON.stringify(luisCoin, null, 4));
+
+luisCoin.chain[1].data = { amount: 100 };
+luisCoin.chain[1].hash = luisCoin.chain[1].calculateHash();
+
+// After tampering with the blockchain, the chain is no longer valid.
+
+console.log('Is blockchain valid?' + luisCoin.isChainValid());
